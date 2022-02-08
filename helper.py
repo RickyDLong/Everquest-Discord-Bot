c19848f53fae58b3e396ef7b15709ff743dfa290
Ricky Long <33757129+RickyDLong@users.noreply.github.com>
2022-02-07 19:09:53 -0600

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,46 @@
+import array as arr
+import csv
+
+#Pastes the garbage raw copied format from wiki.project1999 into an item name only file
+def grabItemNames():
+    file = open('Every 2.txt', "r").readlines()[0::2]
+    with open('../Item List.txt', "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(file)
+
+#Clean up the item names and take out any unneccesary characters
+def cleanUp(fileName):
+    fileIn = open(fileName, 'rt')
+    fileOut = open('../New Output.txt', "wt")
+
+    for line in fileIn:
+        fileOut.write(line.replace('","', ""))
+    fileIn.close()
+    fileOut.close()
+
+#Opens .txt file and places each line item in a set
+#Outputs new file with zero duplicates
+def findDuplicates(fileName):
+    lines_seen = set()
+    outFile = open('../Item List.txt', "wt")
+    inFile = open(fileName, 'rt')
+    for line in inFile:
+        print(line)
+        if(line not in lines_seen): #Not a duplicate line
+            outFile.write(line)
+            lines_seen.add(line)
+    outFile.close()
+
+def extractItemName(fileName):
+    outFile = open('../Item List.txt', "wt")
+    inFile = open(fileName, "rt")
+    for line in inFile:
+        index = line.split(" ")
+
+
+findDuplicates('Item Names.txt')
+
+
+
+
+
